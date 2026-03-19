b2723df75de9bbe2344ecf6575a1f185d55c4f4e
marcie <kangsorom@gmail.com>
2026-03-19 15:43:36 +1000

    phython_190326

--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,10 +1,27 @@
-def move_zeros(lst):
-    zeros = []
-    for num in lst:
-        if num == 0:
-            lst.pop()
-            zeros.append(0)
+def simple_assembler(program):
+    registers = {}
+    pc = 0
+    splited_program = []
 
-    return lst + zeros
+    for part in program:
+        splited_program += [part.split(" ")]
 
-print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
+    while pc < len(program):
+        command = splited_program[pc][0]
+        x = splited_program[pc][1]
+
+        if command == "mov":
+            registers[x] = int(splited_program[pc][2])
+        elif command == "inc":
+            registers[x] += 1
+        elif command == "dec":
+            registers[x] -= 1
+        elif command == "jnz" and registers[x] != 0:
+            pc -= int(splited_program[pc][2])
+            continue
+
+        pc += 1
+
+    return registers
+
+print(simple_assembler(["mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a"]))
