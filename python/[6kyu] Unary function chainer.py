c487df4f2bf1a618002c8629aeb05742fa771c78
marcie <kangsorom@gmail.com>
2026-04-14 14:42:08 +1000

    python_140426

---
+++ python/[6kyu] Unary function chainer.py
@@ -0,0 +1,18 @@
+"""
+Your task is to write a higher order function for chaining together a list of unary functions. In other words, it should return a function that does a left fold on the given functions.
+
+chained([a,b,c,d])(input)
+Should yield the same result as
+
+d(c(b(a(input))))
+"""
+
+def chained(functions):
+    def func(input):
+        result = input
+
+        for function in functions:
+            result = function(result)
+
+        return result
+    return func
