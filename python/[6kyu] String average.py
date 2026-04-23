beb29459b8e3f0103a76d0d8c2eb64500ab5c7b9
marcie <kangsorom@gmail.com>
2026-04-23 14:46:16 +1000

    python_230426

---
+++ python/[6kyu] String average.py
@@ -0,0 +1,22 @@
+"""
+You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
+
+"zero nine five two" -> "four"
+
+If the string is empty or includes a number greater than 9, return "n/a"
+"""
+NUMBER = {
+    "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
+    "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9
+}
+
+def average_string(s):
+    operands = s.split(" ")
+
+    for number in operands:
+        if number not in NUMBER:
+            return "n/a"
+
+    average = sum(NUMBER[number] for number in operands) // len(operands)
+
+    return list(NUMBER.keys())[average]
