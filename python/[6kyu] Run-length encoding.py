6f08d7f10aec19d79cd80b77b69d6515b7168bc5
marcie <kangsorom@gmail.com>
2026-05-15 17:04:20 +1000

    python_150526

---
+++ python/[6kyu] Run-length encoding.py
@@ -0,0 +1,35 @@
+"""
+Description:
+Run-length encoding (RLE) is a very simple form of data compression in which runs of data (that is, sequences in which the same data value occurs in many consecutive data elements) are stored as a single data value and count, rather than as the original run. Wikipedia
+
+Task
+Your task is to write such a run-length encoding. For a given string, return a list (or array) of pairs (or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct the original string by replicating the character sx ix times and concatening all those strings. Your run-length encoding should be minimal, ie. for all i the values si and si+1 should differ.
+
+Examples
+As the article states, RLE is a very simple form of data compression. It's only suitable for runs of data, as one can see in the following example:
+
+run_length_encoding("hello world!")
+ //=>      [[1,'h'], [1,'e'], [2,'l'], [1,'o'], [1,' '], [1,'w'], [1,'o'], [1,'r'], [1,'l'], [1,'d'], [1,'!']]
+It's very effective if the same data value occurs in many consecutive data elements:
+
+run_length_encoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
+# => [[34,'a'], [3,'b']]
+"""
+
+def run_length_encoding(s):
+    if not s:
+        return []
+
+    result = []
+    count = 1
+
+    for index in range(len(s) - 1):
+        if s[index] == s[index + 1]:
+            count += 1
+        else:
+            result.append(count, s[index])
+            count = 1
+
+    result.append([count, s[-1]])
+
+    return result
