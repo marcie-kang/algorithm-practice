bf769c0ac8a9b37846f88ca8350650682b148030
marcie <kangsorom@gmail.com>
2026-05-15 16:00:16 +1000

    python_150526

---
+++ python/[6kyu] Sort Arrays (Ignoring Case).py
@@ -0,0 +1,9 @@
+"""
+Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+"""
+
+def sortme(words):
+    return sorted(words, key=str.lower)
