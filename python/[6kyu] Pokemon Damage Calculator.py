10217a6ecc67db9c65fa523e1b6cfdff12749d18
marcie <kangsorom@gmail.com>
2026-06-01 17:24:39 +1000

    python_010626

---
+++ python/[6kyu] Pokemon Damage Calculator.py
@@ -0,0 +1,69 @@
+"""
+It's a Pokemon battle! Your task is to calculate the damage that a particular move would do using the following formula (not the actual one from the game):
+
+damage = 50 * (attack / defense) * effectiveness
+Where:
+
+attack = your attack power
+defense = the opponent's defense
+effectiveness = the effectiveness of the attack based on the matchup (see explanation below)
+Effectiveness:
+
+Attacks can be super effective, neutral, or not very effective depending on the matchup. For example, water would be super effective against fire, but not very effective against grass.
+
+Super effective: 2x damage
+Neutral: 1x damage
+Not very effective: 0.5x damage
+To prevent this kata from being tedious, you'll only be dealing with four types: fire, water, grass, and electric. Here is the effectiveness of each matchup:
+
+fire > grass
+
+fire < water
+
+fire = electric
+
+water < grass
+
+water < electric
+
+grass = electric
+
+For this kata, any type against itself is not very effective. Also, assume that the relationships between different types are symmetric (if A is super effective against B, then B is not very effective against A).
+
+The function you must implement takes in:
+
+your type
+the opponent's type
+your attack power
+the opponent's defense
+"""
+
+
+def calculate_damage(your_type, opponent_type, attack, defense):
+    if your_type == opponent_type:
+        effectiveness = 0.5
+
+    else:
+        relations = {
+            ("fire", "grass"): 2,
+            ("fire", "water"): 0.5,
+            ("fire", "electric"): 1,
+            ("water", "grass"): 0.5,
+            ("water", "electric"): 0.5,
+            ("grass", "electric"): 1
+        }
+
+        if (your_type, opponent_type) in relations:
+            effectiveness = relations[(your_type, opponent_type)]
+        elif (opponent_type, your_type) in relations:
+            opposite_eff = relations[(opponent_type, your_type)]
+            if opposite_eff == 2:
+                effectiveness = 0.5
+            elif opposite_eff == 0.5:
+                effectiveness = 2
+            else:
+                effectiveness = 1
+
+    damage = 50 * (attack / defense) * effectiveness
+    
+    return damage
