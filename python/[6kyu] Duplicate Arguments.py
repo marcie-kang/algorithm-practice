d32ec9db32f45ce93f5971218cd9d9e284e33a7d
marcie <kangsorom@gmail.com>
2026-06-04 23:04:06 +1000

    python_040626

---
+++ python/[6kyu] Duplicate Arguments.py
@@ -0,0 +1,15 @@
+"""
+Complete the solution so that it returns true if it contains any duplicate argument values, and false otherwise. Any number of arguments may be passed into the function.
+
+The arguments passed in will only be strings or numbers.
+
+Examples:
+
+solution(1, 2, 3)             -->  false
+solution(1, 2, 3, 2)          -->  true
+solution('1', '2', '3', '2')  -->  true
+"""
+
+def solution(*args):
+    return len(args) != len(set(args))
+
