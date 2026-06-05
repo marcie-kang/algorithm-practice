1359cc506e6ecbada3874908d6db1fdb3a3a7a45
marcie <kangsorom@gmail.com>
2026-06-05 12:26:10 +1000

    python_050626

---
+++ python/[6kyu] Schrödinger's Boolean.py
@@ -0,0 +1,13 @@
+"""
+Can a value be both True and False?
+
+Define omnibool so that it returns True for the following:
+
+omnibool == True and omnibool == False
+"""
+
+class Omnibool:
+    def __eq__(self, other):
+        return True
+
+omnibool = Omnibool()
